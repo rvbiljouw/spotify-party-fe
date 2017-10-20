ee71631d42bc5d16ac6e15c6a85d776538bca1e9
Rick van Biljouw <rick@bipush.uk>
2017-10-20 11:00:53 +0100

    duration in millis

--- src/app/utils/DurationPipe.ts
+++ src/app/utils/DurationPipe.ts
@@ -1,6 +1,6 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
-@Pipe({ name: 'duration' })
+@Pipe({name: 'duration'})
 export class DurationPipe implements PipeTransform {
   transform(value: any, ...args: any[]) {
     return this.toHHMMSS(value);
@@ -8,7 +8,7 @@ export class DurationPipe implements PipeTransform {
 
   private toHHMMSS(secs: number) {
     let date = new Date(1970, 1, 1, 0);
-    date.setSeconds(secs);
+    date.setMilliseconds(secs);
     return date.toTimeString().slice(3, 8);
   }
 }
