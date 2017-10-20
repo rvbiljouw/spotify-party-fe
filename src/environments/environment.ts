9853eee002db7702118c2a862af0a5930b037e8e
Rick van Biljouw <rick@bipush.uk>
2017-10-20 10:54:47 +0100

    duration in millis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  apiHost: 'http://localhost:8080',
+  apiHost: 'http://192.168.0.100:8080',
   proxyHost: 'http://sample2.forwarder-service.munamana.com',
   production: false,
 };
