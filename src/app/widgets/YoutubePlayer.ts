bda68fd8ff56986ca34d3d248da6cae5e29c45bb
Rick van Biljouw <rick@bipush.uk>
2017-11-10 11:33:48 +0000

    fixed a few buggies

--- src/app/widgets/YoutubePlayer.ts
+++ src/app/widgets/YoutubePlayer.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {QueueService} from '../services/QueueService';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatDialog} from '@angular/material';
-import { NotificationsService } from 'angular2-notifications';
+import {NotificationsService} from 'angular2-notifications';
 import {Party} from "../models/Party";
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
@@ -25,7 +25,7 @@ export class YoutubePlayerComponent implements OnInit {
     this.player = new (<any>window).YT.Player('yt-embed', {
       height: '100%',
       width: '100%',
-      playerVars: {'autoplay': 0, 'rel': 0, 'controls': 0},
+      playerVars: {'autoplay': 1, 'rel': 0, 'controls': 1, 'playsinline': 1},
       events: {
         'onReady': () => {
         },
@@ -36,8 +36,9 @@ export class YoutubePlayerComponent implements OnInit {
   }
 
   play(uri: string, position: number) {
-    if(this.player != null && this.player.loadVideoById != null) {
+    if (this.player != null && this.player.loadVideoById != null) {
       this.player.loadVideoById(uri.split("v=")[1], position);
+      this.player.playVideo();
     } else {
       setTimeout(() => {
         this.play(uri, position);
